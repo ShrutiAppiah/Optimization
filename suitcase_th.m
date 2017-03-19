a23b05cfab0777ca1c7675f2b944aeb4d281f2ea
Shruti Appiah <shruti.appiah@gmail.com>
2017-03-19 11:17:56 -0400

    setting all vars

--- suitcase_th.m
+++ suitcase_th.m
@@ -1,15 +1,35 @@
-% Material properties
-% Density of lining material [kg/m^3]
-% Area of the suitcase face [m^2], 
-% Young's Modulus of the lining material and shell [kPa]
-% Thickness of shell [m]
-AREA = 0.76*0.51;
-DENSITY = 32.84;
-YOUNGS_MOD_L = 27.578;
-DAMPING_RATIO_L = 0.096;
-YOUNGS_MOD_S = 2.39e6;
-TH_S = 0.025;
+% Set resolution of optimization for incremental thickness
+RES = 100;
 
-% Minor calculations
-m_s = AREA * TH_S  %Mass of shell
+% Material properties
+DENSITY_L = 32.84;          % Density of lining material [kg/m^3]
+YOUNGS_MOD_L = 27.578;      % Young's Modulus of the lining material [kPa]
+DAMPING_RATIO_L = 0.096;    % Damping ratio of lining 
+YOUNGS_MOD_S = 2.39e6;      % Young's Modulus of the shell [kPa]
+
+%% Shell and contents
+% Fixed parameters
+AREA = 0.76*0.51;               % Area of the suitcase face [m^2], 
+m_c = 34.5;
+k_s = YOUNGS_MOD_S*AREA/TH_S;
+b_s = 0;                        % Assume no damping in the shell material
+TH_S = 0.025;                   % Thickness of shell [m]
+
+% Minor preliminary calculations
+m_s = AREA * TH_S               %Mass of shell
+
+
+%% Lining
+% Thickness of lining
+min_th = 0;
+max_th = 0.014;
+dth = (max_th - min_th)/RES;
+th = max_th;                    % Start with maximum thickness 
+
+% Other parameters 
+v_l = AREA*th
+m_l = DENSITY_L*v_l
+k_l = YOUNGS_MOD_S*AREA/th;
+b_l = DAMPING_RATIO_L*2*sqrt(k_l*m_l);
+
 
